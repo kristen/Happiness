f8c49b3ae7fb8c4e06afe206f1effc37c1958526
Kristen Sundquist <kristen@csc8.com>
2012-07-14 15:32:33 -0700

    added draw face to drawRect

--- Happiness/FaceView.m
+++ Happiness/FaceView.m
@@ -28,16 +28,29 @@
     UIGraphicsPopContext();
 }
 
+#define DEFAULT_SIZE 0.90
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
     CGContextRef context = UIGraphicsGetCurrentContext();
-    
+
     // draw face (circle)
     
+    CGPoint midPoint;
+    midPoint.x = self.bounds.origin.x + self.bounds.size.width/2;
+    midPoint.y = self.bounds.origin.y + self.bounds.size.height/2;
+    
+    CGFloat size = self.bounds.size.width/2;
+    if (self.bounds.size.height < self.bounds.size.width) size = self.bounds.size.height/2;
+    size *= DEFAULT_SIZE;
+    
+    CGContextSetLineWidth(context, 5.0);
+    [[UIColor blueColor] setStroke];
+    [self drawCircleAtPoint:midPoint withRadius:size inContext:context];
+    
     // draw eyes (2 circles)
+    
+    
     
     //draw mouth
 }
