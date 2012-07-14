60182da2cfae699d4a7b385a47793c305da1b0df
Kristen Sundquist <kristen@csc8.com>
2012-07-14 15:14:16 -0700

    added FaceView class

---
+++ Happiness/FaceView.m
@@ -0,0 +1,31 @@
+//
+//  FaceView.m
+//  Happiness
+//
+//  Created by Kristen Sundquist on 7/14/12.
+//  Copyright (c) 2012 Me. All rights reserved.
+//
+
+#import "FaceView.h"
+
+@implementation FaceView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
