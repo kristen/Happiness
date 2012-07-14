90f5e455ee53158d2208aad3208854dfb8a6ab74
Kristen Sundquist <kristen@csc8.com>
2012-07-14 15:11:42 -0700

    added property happiness

--- Happiness/HappinessViewController.m
+++ Happiness/HappinessViewController.m
@@ -8,27 +8,10 @@
 
 #import "HappinessViewController.h"
 
-@interface HappinessViewController ()
 
-@end
 
 @implementation HappinessViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-}
+@synthesize happiness = _happiness;
 
 @end
